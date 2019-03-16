51cbe805bb0700147e1074f2d5d3052329cbc617
Abidujjaman Ovi <oviice@gmail.com>
2019-03-16 15:27:38 +0600

    Sqlite

---
+++ app/src/main/java/com/ovi/sqlitedemo/DatabaseSqlite.java
@@ -0,0 +1,52 @@
+package com.ovi.sqlitedemo;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseSqlite extends SQLiteOpenHelper {
+    public static final String DataBase_Name="Student.db";
+    public static final String TABLE_NAME = "student_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "PhnNumber";
+    public static final int DbVersion=1;
+
+    DatabaseSqlite(Context context){
+        super(context,DataBase_Name,null,DbVersion);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("Create Table "+TABLE_NAME+ "(ID INTEGER PRIMARY KEY ,NAME TEXT,PhnNumber TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("Drop table if EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String id,String name,String PhnNumber){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues contentValues=new ContentValues();
+        contentValues.put(COL_1,id);
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,PhnNumber);
+        long results=db.insert(TABLE_NAME,null,contentValues);
+        return true;
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
+        return res;
+    }public Cursor Delete() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
+        return res;
+    }
+
+}
---
+++ app/src/main/java/com/ovi/sqlitedemo/MainActivity.java
@@ -0,0 +1,83 @@
+package com.ovi.sqlitedemo;
+
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText id,name,phnNum;
+    Button add,view;
+    DatabaseSqlite myDb;
+TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initiateAll();
+        myDb=new DatabaseSqlite(this);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddData();
+            }
+        });
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewData();
+            }
+        });
+    }
+
+
+
+
+    public void ViewData() {
+        textView.setText("");
+        Cursor res=myDb.getAllData();
+        if(res.getCount() == 0) {
+            // show message
+            textView.setText("No Data Found");
+            return;
+        }
+
+        while (res.moveToNext()){
+            textView.append("ID:"+res.getString(0)+"\n");
+            textView.append("Name:"+res.getString(1)+"\n");
+            textView.append("Phone Number:"+res.getString(2)+"\n\n");
+
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    public void AddData() {
+        boolean isInsert=(myDb.insertData(id.getText().toString(),name.getText().toString(),phnNum.getText().toString()));
+        if (isInsert=true){
+            Toast.makeText(MainActivity.this,"Data insert",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(MainActivity.this,"Data not Insert",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void initiateAll(){
+        id=(EditText)findViewById(R.id.id);
+        name=(EditText)findViewById(R.id.name);
+        phnNum=(EditText)findViewById(R.id.PhoneNumber);
+        add=(Button)findViewById(R.id.addButton);
+        view=(Button)findViewById(R.id.View);
+        textView=findViewById(R.id.textView);
+    }
+
+}
